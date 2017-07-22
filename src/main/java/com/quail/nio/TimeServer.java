a37760b39a3fb1a5c55d8f6fc7779aeaee0784d4
wuyifei <wuyifei>
2017-07-22 14:57:09 +0800

    1.时间服务器-NIO版

--- src/main/java/com/quail/nio/TimeServer.java
+++ src/main/java/com/quail/nio/TimeServer.java
@@ -8,13 +8,9 @@
   Date: 2017-07-10 13:07:08
   Description: 
 *************************************************/
-package main.java.com.quail.paio;
+package com.quail.nio;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import main.java.com.quail.bio.TimeServerHandler;
 
 /**
  * File name: TimeServer.java Description:
@@ -32,24 +28,13 @@ public class TimeServer {
 				// 采用默认值
 			}
 		}
-		ServerSocket server = null;
+
+
 		try {
 
-			server = new ServerSocket(port);
-			System.out.println("The time server is start in port : " + port);
-			Socket socket = null;
 
-			while (true) {
-				socket = server.accept();
-				// 将业务处理线程加入线程池(与BIO版本的区别)
-				new Thread(new TimeServerHandler(socket)).start();
-			}
 		} finally {
-			if (server != null) {
-				System.out.println("The time server close");
-				server.close();
-				server = null;
-			}
+
 		}
 
 	}
